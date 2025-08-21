86d5e9accad134f18dd4835c06004a8fb6ba3f94
taehyun.kim <brianmc@hanmail.net>
2025-08-21 10:15:05 +0000

    feat: DirectorLoader버전(여러파일 읽기)

---
+++ code/baseline_directoryloader.py
@@ -0,0 +1,94 @@
+import os
+from dotenv import load_dotenv
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_community.document_loaders import PyMuPDFLoader
+from langchain_community.vectorstores import FAISS
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.runnables import RunnablePassthrough
+from langchain_core.prompts import PromptTemplate
+from langchain_openai import ChatOpenAI, OpenAIEmbeddings
+from langchain_upstage import ChatUpstage
+from langchain_upstage import UpstageEmbeddings
+ 
+# 현재 스크립트 위치를 작업 디렉토리로 설정
+import sys
+from pathlib import Path
+script_dir = Path(__file__).parent.absolute()
+os.chdir(script_dir)
+
+# 환경변수 로드
+load_dotenv()
+
+
+# 단계 1: 문서 로드(Load Documents)
+from langchain_community.document_loaders import DirectoryLoader
+
+# data/pdf 폴더 및 모든 서브폴더의 PDF 파일을 읽어옴
+pdf_dir = "../data/pdf"
+loader = DirectoryLoader(
+    path=pdf_dir,
+    glob="**/*.pdf",
+    loader_cls=PyMuPDFLoader, # PDF 파일을 읽어오는 로더 클래스
+    show_progress=True, # 진행 상황을 표시할지 여부
+    recursive=True, # 하위 폴더를 재귀적으로 검색할지 여부
+    use_multithreading=True # 멀티스레딩을 사용할지 여부
+)
+docs = loader.load()
+
+# 단계 2: 문서 분할(Split Documents)
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=50)
+split_documents = text_splitter.split_documents(docs)
+
+# 단계 3: 임베딩(Embedding) 생성
+# embeddings = OpenAIEmbeddings()
+embeddings = UpstageEmbeddings(
+    api_key=os.getenv("UPSTAGE_API_KEY"),
+    model="embedding-query"
+)
+
+# 단계 4: DB 생성(Create DB) 및 저장
+# 벡터스토어를 생성합니다.
+vectorstore = FAISS.from_documents(documents=split_documents, embedding=embeddings)
+
+# 단계 5: 검색기(Retriever) 생성
+# 문서에 포함되어 있는 정보를 검색하고 생성합니다.
+retriever = vectorstore.as_retriever()
+
+# 단계 6: 프롬프트 생성(Create Prompt)
+# 프롬프트를 생성합니다.
+prompt = PromptTemplate.from_template(
+    """You are an assistant for question-answering tasks. 
+Use the following pieces of retrieved context to answer the question. 
+If you don't know the answer, just say that you don't know. 
+Answer in Korean.
+
+#Question: 
+{question} 
+#Context: 
+{context} 
+
+#Answer:"""
+)
+
+# 단계 7: 언어모델(LLM) 생성
+# 모델(LLM) 을 생성합니다.
+# llm = ChatOpenAI(model_name="gpt-4o", temperature=0)
+llm = ChatUpstage(
+    api_key=os.getenv("UPSTAGE_API_KEY"),
+    model="solar-pro2",
+    reasoning_effort="high"
+)
+
+# 단계 8: 체인(Chain) 생성
+chain = (
+    {"context": retriever, "question": RunnablePassthrough()}
+    | prompt
+    | llm
+    | StrOutputParser()
+)
+
+# 체인 실행(Run Chain)
+# 문서에 대한 질의를 입력하고, 답변을 출력합니다.
+question = "단팥빵 레시피를 1/4로 양을 줄여서 표로 보여줘"
+response = chain.invoke(question)
+print(response)
