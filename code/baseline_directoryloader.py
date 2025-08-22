f50161cb814936ca2689fe68a794dcd1c85ed18b
taehyun.kim <brianmc@hanmail.net>
2025-08-22 03:53:20 +0000

    feat: 현재시간을 프롬프트에 반영

--- code/baseline_directoryloader.py
+++ code/baseline_directoryloader.py
@@ -10,6 +10,8 @@ from langchain_openai import ChatOpenAI, OpenAIEmbeddings
 from langchain_upstage import ChatUpstage
 from langchain_upstage import UpstageEmbeddings
 import utils.log_util as log
+import datetime
+import pytz
  
 # 현재 스크립트 위치를 작업 디렉토리로 설정
 import sys
@@ -67,12 +69,16 @@ retriever = vectorstore.as_retriever()
 log.info("벡터 데이터베이스와 검색기가 성공적으로 생성되었습니다.")
 
 # 단계 6: 프롬프트 생성(Create Prompt)
+# 현재 국가 로케일 기준의 날짜와 시간
+tz = pytz.timezone("Asia/Seoul")
+nowTime = datetime.datetime.now(tz).strftime('%Y-%m-%d %H:%M:%S')
+
 # 프롬프트를 생성합니다.
 prompt = PromptTemplate.from_template(
     """You are an assistant for question-answering tasks. 
 Use the following pieces of retrieved context to answer the question. 
 If you don't know the answer, just say that you don't know. 
-Answer in Korean.
+Answer in Korean. The current time is {nowTime}.
 
 #Question: 
 {question} 
@@ -81,7 +87,7 @@ Answer in Korean.
 
 #Answer:"""
 )
-log.info("프롬프트 템플릿이 생성되었습니다.")
+log.info(f"프롬프트 템플릿이 생성되었습니다. 현재시각: {nowTime}")
 
 # 단계 7: 언어모델(LLM) 생성
 log.info("언어모델을 초기화하는 중...")
@@ -96,7 +102,7 @@ log.info("언어모델이 성공적으로 초기화되었습니다.")
 
 # 단계 8: 체인(Chain) 생성
 chain = (
-    {"context": retriever, "question": RunnablePassthrough()}
+    {"context": retriever, "question": RunnablePassthrough(), "nowTime": lambda x: nowTime}
     | prompt
     | llm
     | StrOutputParser()
@@ -105,7 +111,7 @@ log.info("RAG 체인이 성공적으로 생성되었습니다.")
 
 # 체인 실행(Run Chain)
 # 문서에 대한 질의를 입력하고, 답변을 출력합니다.
-question = "단팥빵 레시피를 1/4로 양을 줄여서 표로 보여줘"
+question = "작년 변경된 실기시험 종목들은 무엇이야?"
 log.info(f"질문: {question}")
 log.info("답변을 생성하는 중...")
 response = chain.invoke(question)
